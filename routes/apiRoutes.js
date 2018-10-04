11d2c170ba399b73a729f00ac1cc7a2ffccae774
Craig Bomba <talk2cbo@gmail.com>
2018-10-03 21:54:54 -0500

    added front end javascript

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,17 +4,18 @@ module.exports = function (app) {
     let cheerio = app.get('cheerio');
     let db = app.get('db');
 
-    app.get("/doscrape", function (req, res) {
+    app.get("/scrape", function (req, res) {
         // First, we grab the body of the html with request
         axios.get("http://www.reuters.com/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
+            let scraped = [];
             // Now, we grab every h3 within an article tag, and do the following:
             $("article h3").each(function (i, element) {
                 console.log($(this).children("a").text());
                 // Save an empty result object
-                var result = {};
+                let result = {};
 
                 // Add the text and href of every link, and save them as properties of the result object
                 result.title = $(this)
@@ -24,23 +25,77 @@ module.exports = function (app) {
                     .children("a")
                     .attr("href");
 
-                // Create a new Article using the `result` object built from scraping
-                db.Article.create(result)
-                    .then(function (dbArticle) {
-                        // View the added result in the console
-                        console.log(dbArticle);
-                    })
-                    .catch(function (err) {
-                        // Errors, including violating a duplicate (unique constraint violation)
-                        // should be logged, but I doubt they should be sent back to the UI.
-                        console.log(`ERROR ----> ${err}`);
-                        // return res.json(err);
-                    });
+                if (result.title != "") {  // if we have a null article, move on to the next one
+                    scraped.push(result); // add to the array
+
+                    // Create a new Article using the `result` object built from scraping
+                    db.Article.create(result)
+                        .then(function (dbArticle) {
+                            // View the added result in the console
+                            console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                            // Errors, including violating a duplicate (unique constraint violation)
+                            // should be logged, but I doubt they should be sent back to the UI.
+                            console.log(`ERROR ----> ${err}`);
+                            // return res.json(err);
+                        });
+                }
             });
 
             // since we got here (i.e. no exception caught) return with a success message back to the UI
-            res.send("Wala....scrape completed successfully");
+            res.send(scraped);
         });
+    });
+
+    // Route for getting all Articles from the db
+    app.get("/articles", function (req, res) {
+        // Grab every document in the Articles collection
+        db.Article.find({})
+            .then(function (dbArticle) {
+                // If we were able to successfully find Articles, send them back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
+
+    // Route for grabbing a specific Article by id, populate it with it's note
+    app.get("/articles/:id", function (req, res) {
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findOne({ _id: req.params.id })
+            // ..and populate all of the notes associated with it
+            .populate("note")
+            .then(function (dbArticle) {
+                // If we were able to successfully find an Article with the given id, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
+
+    // Route for saving/updating an Article's associated Note
+    app.post("/articles/:id", function (req, res) {
+        // Create a new note and pass the req.body to the entry
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            })
+            .then(function (dbArticle) {
+                // If we were able to successfully update an Article, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
     });
 
 
