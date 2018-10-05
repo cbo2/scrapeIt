328522c554986163caf8886d32735fd288f96eba
Craig Bomba <talk2cbo@gmail.com>
2018-10-05 07:43:30 -0500

    small fix to associated notes with the article

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -88,9 +88,8 @@ module.exports = function (app) {
             .then(function (dbNote) {
                 console.log("the note should now be in the database with: " + JSON.stringify(dbNote));
                 // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+                // { new: true } tells the query that we want it to return the updated Article instead of the original by default
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { notes: dbNote._id }, { new: true });
             })
             .then(function (dbArticle) {
                 // If we were able to successfully update an Article, send it back to the client
